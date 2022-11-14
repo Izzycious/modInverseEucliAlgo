898cc0c8588de5cacd862c78b66e9a99b92d61ad
Ishaq Nasir <ishaqnasir47@gmail.com>
2022-11-14 08:50:58 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-fn mod_in(a: isize, module: isize) -> isize {
+fn mod_inv(a: isize, module: isize) -> isize {
     let mut ab = (module, a);
-    let mut xy = (0,1);
-
-    while ab != 0 {
-        xy = (xy.1, xy.0 - (ab.0/mn.1) xy.1);
-        ab = (ab.1, ab.0 % ab.1);
+    let mut xy = (0, 1);
+   
+    while ab.1 != 0 {
+      xy = (xy.1, xy.0 - (ab.0/ab.1) * xy.1);
+      ab = (ab.1, ab.0 % ab.1);
     }
-}
+
+    while xy.0 < 0 {
+      xy.0 += module;
+    }
+    xy.0
+  }
+
+  fn main() {
+    println!("{}", mod_inv(29, 90)) 
+  }
