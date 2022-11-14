917585ec940045537fd74b32c0eb66c3354b6eff
Ishaq Nasir <ishaqnasir47@gmail.com>
2022-11-14 09:27:00 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-fn mod_inv(a: isize, module: isize) -> isize {
+/*
+Declaring a function that **modInverse** that takes in a 
+ (a and module) of 
+ isize(which is either 32/64bits) depending on the sytem.
+
+ In rust **isize** differentiate from **usize** such that
+ isize:  can be negative, and is generally used for offsets to addresses, positions, indices, or lengths.
+         and in the code below we're dealing with index numbers
+ usize:  cannot be negative and is generally used for memory addresses, positions, indices, lengths (or sizes!).
+*/ 
+
+fn mod_inverse(a: isize, module: isize) -> isize {
     let mut ab = (module, a);
     let mut xy = (0, 1);
+
+    
    
     while ab.1 != 0 {
       xy = (xy.1, xy.0 - (ab.0/ab.1) * xy.1);
@@ -14,5 +27,5 @@ fn mod_inv(a: isize, module: isize) -> isize {
   }
 
   fn main() {
-    println!("{}", mod_inv(29, 90)) 
+    println!("{}", mod_inverse(29, 90)) 
   }
